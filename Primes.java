c2246dd18b0fb8fcddefbe9a70194570a1e2ec4c
omer <omermaayan@icloud.com>
2025-11-24 19:07:05 +0200

    update

--- Primes.java
+++ Primes.java
@@ -1,5 +1,29 @@
 public class Primes {
     public static void main(String[] args) {
         // Replace this statement with your code
+        int maximumPrime = Integer.parseInt(args[0]);
+        int primeCount = 0;
+
+        for (int i = 2; i <= maximumPrime; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {  
+                    isPrime = false;
+                    break;           
+                }
+            }
+
+            if (isPrime) {
+                primeCount++;
+                System.out.println(i);
+            }
+        }
+
+        int percent = (int) Math.round(100.0 * primeCount / maximumPrime);
+
+        System.out.println("There are " + primeCount +
+                " primes between 2 and " + maximumPrime +
+                " (" + percent + "% are primes)");
     }
 }
